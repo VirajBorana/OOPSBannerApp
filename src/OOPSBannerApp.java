3d3acdacbd32c67cc7f2205943a4754906bec95b
VirajBorana <vb9731@srmist.edu.in>
2026-02-17 13:54:46 +0530

    UC3 Refactored banner using methods

--- src/OOPSBannerApp.java
+++ src/OOPSBannerApp.java
@@ -1,14 +1,40 @@
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
+        printLine1();
+        printLine2();
+        printLine3();
+        printLine4();
+        printLine5();
+        printLine6();
+        printLine7();
+    }
 
+    static void printLine1() {
         System.out.println(" *****     *****     ******    ***** ");
-        System.out.println("*     *   *     *    *     *  *     *");
-        System.out.println("*     *   *     *    *     *  *      ");
-        System.out.println("*     *   *     *    ******    ***** ");
-        System.out.println("*     *   *     *    *              *");
-        System.out.println("*     *   *     *    *        *     *");
-        System.out.println(" *****     *****     *         ***** ");
+    }
 
+    static void printLine2() {
+        System.out.println("*     *   *     *    *     *  *     *");
+    }
+
+    static void printLine3() {
+        System.out.println("*     *   *     *    *     *  *      ");
+    }
+
+    static void printLine4() {
+        System.out.println("*     *   *     *    ******    ***** ");
+    }
+
+    static void printLine5() {
+        System.out.println("*     *   *     *    *              *");
+    }
+
+    static void printLine6() {
+        System.out.println("*     *   *     *    *        *     *");
+    }
+
+    static void printLine7() {
+        System.out.println(" *****     *****     *         ***** ");
     }
 }
