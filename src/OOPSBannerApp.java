dbedbccba86eef7c8068cda93029ff168679c916
VirajBorana <vb9731@srmist.edu.in>
2026-02-24 13:15:00 +0530

    Implemented UC3 using String.join() as per requirement

--- src/OOPSBannerApp.java
+++ src/OOPSBannerApp.java
@@ -1,40 +1,47 @@
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
-        printLine1();
-        printLine2();
-        printLine3();
-        printLine4();
-        printLine5();
-        printLine6();
-        printLine7();
-    }
 
-    static void printLine1() {
-        System.out.println(" *****     *****     ******    ***** ");
-    }
+        System.out.println(String.join(" ",
+                " *****  ",
+                " *****  ",
+                " ****** ",
+                " *****  "));
 
-    static void printLine2() {
-        System.out.println("*     *   *     *    *     *  *     *");
-    }
+        System.out.println(String.join(" ",
+                "*     * ",
+                "*     * ",
+                "*     * ",
+                "*     * "));
 
-    static void printLine3() {
-        System.out.println("*     *   *     *    *     *  *      ");
-    }
+        System.out.println(String.join(" ",
+                "*     * ",
+                "*     * ",
+                "*     * ",
+                "*      "));
 
-    static void printLine4() {
-        System.out.println("*     *   *     *    ******    ***** ");
-    }
+        System.out.println(String.join(" ",
+                "*     * ",
+                "*     * ",
+                "******  ",
+                " *****  "));
 
-    static void printLine5() {
-        System.out.println("*     *   *     *    *              *");
-    }
+        System.out.println(String.join(" ",
+                "*     * ",
+                "*     * ",
+                "*       ",
+                "      * "));
 
-    static void printLine6() {
-        System.out.println("*     *   *     *    *        *     *");
-    }
+        System.out.println(String.join(" ",
+                "*     * ",
+                "*     * ",
+                "*       ",
+                "*     * "));
 
-    static void printLine7() {
-        System.out.println(" *****     *****     *         ***** ");
+        System.out.println(String.join(" ",
+                " *****  ",
+                " *****  ",
+                "*       ",
+                " *****  "));
     }
 }
